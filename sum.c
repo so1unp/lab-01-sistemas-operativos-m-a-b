334698b40d78ba2586a099853b243d35d1a004b2
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2025-03-19 15:45:33 -0300

    Ejercicio 1 termiando

--- sum.c
+++ sum.c
@@ -3,8 +3,25 @@
 
 int main(int argc, char *argv[])
 {
+    int sum = 0;
     // Agregar código aquí.
-
+    if (argc > 1)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            sum += atoi(argv[i]);
+        }
+    }
+    else
+    {
+        int num = 0;
+        do
+        {
+            num = getchar();
+            sum += atoi((char *)&num);
+        } while (num != EOF);
+        }
+    printf("La suma es: %d\n", sum);
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
