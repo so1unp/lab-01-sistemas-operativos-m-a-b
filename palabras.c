64523fabb5549bcd203612388e48e57ac35a2607
Brandon Amarillo <brandonxdamarillo@gmail.com>
2025-03-20 18:32:03 -0300

    Ejercicio 1 y 2 terminado

--- palabras.c
+++ palabras.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main()
 {
     // Agregar código aquí.
+    int c;
+    char buffer[1024];
+    int index = 0; 
 
+    while( (c = getchar()) != EOF) {
+        if(c == ' '|| c == '\t' || c == '\n'){ //Compara si es un separador
+            if(index > 0){
+                buffer[index] = '\0'; // Termina la palabra
+                printf("%s\n", buffer);
+                index = 0;
+            }
+        } else{
+            if(index < 1023){
+                buffer[index++] = (char)c;
+            } else{
+                fprintf(stderr, "Error\n"); // si se rebalsa
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    // Cuando llega a la ultima palabra
+    if (index > 0) { 
+        buffer[index] = '\0';
+        printf("%s\n", buffer);
+    } 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
--- sum.c
+++ sum.c
@@ -4,7 +4,19 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-
+    int result = 0;
+    if(argc > 1){
+        for(int i = 0; i < argc; i++){
+            result += atoi(argv[i]);
+        }
+    } else {
+        int c;
+        result = 0;
+        while ((c = getchar()) != EOF) {
+            result += atoi((char *)&c);
+        }
+    } 
+    printf("%d\n",result);
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
