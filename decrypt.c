9cef8b1277180289e5b72ad4cef0e77744cfb7d0
AgustinMed <medina.4gus@gmail.com>
2025-03-23 14:00:07 -0300

    terminado el decrypt

--- decrypt.c
+++ decrypt.c
@@ -4,55 +4,36 @@
 #include <unistd.h>
 
 void decrypt_from_command_line(char *message) {
-
     char word[100] = {};
     int j = 0;
 
     for (int i = 0; message[i] != '\0'; i++) {
-        
-        if ((i+1) % 8 == 0) {
+        if ((i + 1) % 8 == 0) {
             word[j] = message[i];
             j++;
         }
-
     }
 
-    for (int i = 0; i < 8; i++) {
-        printf("%c", word[i]);
-    }
+    word[j] = '\0';
 
+    printf("%s\n", word);
 }
 
 void decrypt_from_file(char *filename) {
-    char word[100] = {};
-    char buffer[1024];
-    int j = 0;
     int exit = open(filename, O_RDONLY);
-    int readed = 0;
-
-    // while((readed = read(exit,buffer,sizeof(buffer))) > 0)
-    // {   
-    //     if ((readed+1) % 8 == 0) {
-    //         if (j < sizeof(word) - 1) {
-    //             word[j] = buffer[readed];
-    //             j++;
-    //         }
-    //     }
-    // }
-
-    while ((readed = read(exit, buffer, sizeof(buffer))) > 0) {
-        for (int i = 0; i < readed; i++) {
-            if ((i+1) % 8 == 0) {
-                if (j < (sizeof(word) - 1)) {
-                    word[j] = buffer[i];
-                    j++;
-                }
-            }
-        }
+    if (exit == -1) {
+        perror("Error al abrir el archivo");
+        return;
     }
-    
-    for (int i = 0; i < 8; i++) {
-        printf("%c", word[i]);
+
+    int word_buffer[8];
+    int readed;
+    while ((readed = read(exit, word_buffer, sizeof(word_buffer))) > 0) {
+        printf("%c", word_buffer[7]);
+    }
+
+    if (readed == -1) {
+        perror("Error al leer el archivo");
     }
 
     close(exit);
@@ -60,15 +41,13 @@ void decrypt_from_file(char *filename) {
 
 int main(int argc, char *argv[])
 {
-    
-    // if (argc == 2) {
-    //     decrypt_from_command_line(argv[1]);
-    // } else {
-    decrypt_from_file(argv[1]);
-    // }
-
-
-    exit(EXIT_SUCCESS);
+    int exit = open(argv[1], O_RDONLY);
+    if (exit == -1) {
+        decrypt_from_command_line(argv[1]);
+        
+    } else {
+        decrypt_from_file(argv[1]);
+    }
 }
 
 
