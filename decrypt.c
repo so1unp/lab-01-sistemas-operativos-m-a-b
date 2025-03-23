8b89bc1329c6a10661cace8a92849b53e93d4dce
Brandon Amarillo <brandonxdamarillo@gmail.com>
2025-03-23 16:47:44 -0300

    Avances con el ejercicio_3

--- decrypt.c
+++ decrypt.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+#define RANDOM_BYTES 7 // Definimos la cantidad de bytes aleatorios que se van a generar
+#define MAX_SIZE_TEXT_ENCRYPT 8 // Definimos el tamaño del texto encriptado
+
+// prototipos
+void decrypt_file(char *filename);
 
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-
+    if( argc == 2){
+        decrypt_file(argv[1]);
+    }
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
+}
+
+void decrypt_file(char *filename){
+
+    // abrimos el archivo
+    int file = open(filename,O_RDONLY);
+    
+    char buffer[1024];
+    ssize_t readed;
+    while ((readed = read(file, buffer, MAX_SIZE_TEXT_ENCRYPT)) > 0) {
+        if (readed == MAX_SIZE_TEXT_ENCRYPT) { // Si se lee un bloque completo
+            printf("%c", buffer[7]); // Extraemos el ultimo byte
+        }
+    }
+    
+    printf("\n");
+    close(file);
 }
--- encrypt.c
+++ encrypt.c
@@ -1,10 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+#define RANDOM_BYTES 7 // Definimos la cantidad de bytes aleatorios que se van a generar
+#define MAX_SIZE_TEXT_ENCRYPT 8 // Definimos el tamaño del texto encriptado
+
+// prototipos
+void encrypt_text(char *message);
+
+void encrypt_file(char *filename);
+
+void encrypt_file_more_text(char *filename, char *message);
+
+void show_message_encrypt(char *message_encrypt);
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
+    srand((unsigned int) time(NULL)); // inicalizamos la semilla para el random
 
+    if(argc == 2){
+        int fileopen = open(argv[1], O_RDONLY);
+        if(fileopen != -1){
+            close(fileopen);
+            encrypt_file(argv[1]); // si es un archivo
+        } else{
+            encrypt_text(argv[1]);// si es un texto
+        }
+        
+    } else if(argc == 3){
+        encrypt_file_more_text(argv[1], argv[2]); //si es un archivo mas el texto
+    }
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
+
+void encrypt_text(char *message){
+
+
+    char random[MAX_SIZE_TEXT_ENCRYPT];
+
+    while(*message != '\0'){
+        for(int i = 0; i < RANDOM_BYTES; i++){
+            random[i] = (char) (rand() % (126 - 32 + 1)) + 32; // generamos un byte aleatorio
+        }
+        random[7] = *message;
+        
+        show_message_encrypt(random);
+
+        message++; // avanzamos al siguiente caracter
+    } 
+    printf("\n"); 
+}
+
+void encrypt_file(char *filename){
+    int file = open(filename,O_RDONLY);
+
+    if (file == -1) {
+        perror("Error al abrir el archivo");
+        exit(EXIT_FAILURE);
+    }
+    
+    char random[MAX_SIZE_TEXT_ENCRYPT];
+    char buffer[1024];
+    ssize_t readed;
+
+    while((readed = read(file, buffer, sizeof(buffer))) > 0){
+        for(ssize_t i = 0; i < readed; i++){
+            for(int j = 0; j < RANDOM_BYTES; j++){
+                random[j] = (char) (rand() % (126 - 32 + 1)) + 32; // generamos un byte aleatorio 
+            }
+            random[7] = buffer[i];
+            show_message_encrypt(random);
+        }
+    }
+    printf("\n");
+    close(file);
+}
+
+void encrypt_file_more_text(char *filename, char *message){
+    (void) filename;
+    (void) message;
+}
+
+void show_message_encrypt(char *message_encrypt){
+
+    for(int i = 0; i < MAX_SIZE_TEXT_ENCRYPT; i++){
+        printf("%c", message_encrypt[i]);
+    }
+}
+
