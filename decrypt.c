89f23656889196eb29a46d55c9ddfd246f90394c
Brandon Amarillo <brandonxdamarillo@gmail.com>
2025-03-26 14:58:31 -0300

    cambios en decrypt

--- decrypt.c
+++ decrypt.c
@@ -8,19 +8,46 @@
 #define MAX_SIZE_TEXT_ENCRYPT 8 // Definimos el tamaño del texto encriptado
 
 // prototipos
-void decrypt_file(char *filename);
+void descrypt_text(char *message);
+
+void descrypt_file(char *filename);
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
-    if( argc == 2){
-        decrypt_file(argv[1]);
+    if(argc == 2){
+        int fileopen = open(argv[1], O_RDONLY);
+        if(fileopen != -1){
+            close(fileopen);
+            descrypt_file(argv[1]); // si es un archivo
+        } else{
+            descrypt_text(argv[1]);// si es un texto
+        } 
     }
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
+/*
+* Función que desencripta un texto
+*/
+void descrypt_text(char *message){
+    char descrypt_arr[1024];
+    int index = 0;
 
-void decrypt_file(char *filename){
+    while(*message != '\0'){
+        descrypt_arr[index++] = message[MAX_SIZE_TEXT_ENCRYPT];
+        printf("%c",message[MAX_SIZE_TEXT_ENCRYPT]);
+        message += MAX_SIZE_TEXT_ENCRYPT;
+    }
+
+    descrypt_arr[index] ='\0';
+    printf("\n");
+
+    printf("%s",descrypt_arr);
+
+    printf("\n");
+}
+
+void descrypt_file(char *filename){
 
     // abrimos el archivo
     int file = open(filename,O_RDONLY);
